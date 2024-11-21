ace4d33f1f1563174bb16929cca5d2f329aa13e2
Kyle Matoba <22180455+kylematoba@users.noreply.github.com>
2024-11-21 08:28:54 +0000

    Log against iteration_step

--- src/nanotron/trainer.py
+++ src/nanotron/trainer.py
@@ -528,8 +528,9 @@ class DistributedTrainer:
                         wandb.log(
                             {
                                 **{log_item.tag: log_item.scalar_value for log_item in log_entries},
-                                "iteration_step": self.iteration_step,
-                            }
+                                # "iteration_step": self.iteration_step,
+                            },
+                            step=self.iteration_step
                         )
         
                 # Checkpoint
@@ -719,8 +720,9 @@ class DistributedTrainer:
                 wandb.log(
                     {
                         **{log_item.tag: log_item.scalar_value for log_item in log_entries},
-                        "iteration_step": self.iteration_step,
-                    }
+                        # "iteration_step": self.iteration_step,
+                    },
+                    step=self.iteration_step
                 )
 
             self.loggerwriter.add_scalars_from_list(log_entries, self.iteration_step)
